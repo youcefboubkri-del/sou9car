161031f3043f7cbc2c725246d671711fd1b3a3c8
Youssef Boubkri <youcefboubkri@gmail.com>
2026-06-22 18:08:53 +0100

    Make Prisma client lazy via Proxy, force-dynamic on admin route

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -11,6 +11,15 @@ function createPrismaClient() {
   return new PrismaClient({ adapter });
 }
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+function getPrisma(): PrismaClient {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = createPrismaClient();
+  }
+  return globalForPrisma.prisma;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_, prop) {
+    return (getPrisma() as any)[prop as string];
+  },
+});
